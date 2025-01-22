8280e9fba00447689d86af23cbdbcca8a076c14f
Rusted2361 <hamzasajid4040@gmail.com>
2025-01-22 14:27:32 +0500

    elevator  solved

--- elevator/src/main.rs
+++ elevator/src/main.rs
@@ -1,8 +1,21 @@
 #[derive(Debug)]
 /// An event in the elevator system that the controller must react to.
 enum Event {
-    // TODO: add required variants
+    /// A button was pressed.
+    ButtonPressed(Button),
+
+    /// The car has arrived at the given floor.
+    CarArrived(Floor),
+
+    /// The car's doors have opened.
+    CarDoorOpened,
+
+    /// The car's doors have closed.
+    CarDoorClosed,
 }
+
+/// A floor is represented as an integer.
+type Floor = i32;
 
 /// A direction of travel.
 #[derive(Debug)]
@@ -11,29 +24,39 @@ enum Direction {
     Down,
 }
 
+/// A user-accessible button.
+#[derive(Debug)]
+enum Button {
+    /// A button in the elevator lobby on the given floor.
+    LobbyCall(Direction, Floor),
+
+    /// A floor button within the car.
+    CarFloor(Floor),
+}
+
 /// The car has arrived on the given floor.
 fn car_arrived(floor: i32) -> Event {
-    todo!()
+    Event::CarArrived(floor)
 }
 
 /// The car doors have opened.
 fn car_door_opened() -> Event {
-    todo!()
+    Event::CarDoorOpened
 }
 
 /// The car doors have closed.
 fn car_door_closed() -> Event {
-    todo!()
+    Event::CarDoorClosed
 }
 
 /// A directional button was pressed in an elevator lobby on the given floor.
 fn lobby_call_button_pressed(floor: i32, dir: Direction) -> Event {
-    todo!()
+    Event::ButtonPressed(Button::LobbyCall(dir, floor))
 }
 
 /// A floor button was pressed in the elevator car.
 fn car_floor_button_pressed(floor: i32) -> Event {
-    todo!()
+    Event::ButtonPressed(Button::CarFloor(floor))
 }
 
 fn main() {
