4012cb0dcac7e42a7d8377b3c8204dba1f187ca6
Rusted2361 <hamzasajid4040@gmail.com>
2025-01-27 13:02:38 +0500

    logger

---
+++ generic-logger/src/main.rs
@@ -0,0 +1,20 @@
+pub trait Logger {
+    /// Log a message at the given verbosity level.
+    fn log(&self, verbosity: u8, message: &str);
+}
+
+struct StderrLogger;
+
+impl Logger for StderrLogger {
+    fn log(&self, verbosity: u8, message: &str) {
+        eprintln!("verbosity={verbosity}: {message}");
+    }
+}
+
+// TODO: Define and implement `VerbosityFilter`.
+
+fn main() {
+    let logger = VerbosityFilter { max_verbosity: 3, inner: StderrLogger };
+    logger.log(5, "FYI");
+    logger.log(2, "Uhoh");
+}
